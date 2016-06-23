7abb2d028b8df53d902e05340d5207d7311fc5b3
Anup Parekh <anup.parekh@aexp.com>
2016-06-23 20:53:02 +0100

    comment

--- client.js
+++ client.js
@@ -1,3 +1,29 @@
+// this block executes when page DOM is ready
+$( document ).ready(function() {
+	
+	connect();	
+	// Listener for click on Enter Game button
+	$('#joinBtn').click(function(e){
+		e.preventDefault();
+		playerName = $('#pname').val();
+		if (playerName == "" ) {
+			console.log("Name was empty");
+		} else {
+			console.log("Registering Player: " + playerName);
+			socket.emit('registerPlayer', playerName, function (data) {
+				if (data) {
+					$('#playerentry').hide();
+					console.log("Player " + playerName + " registered");
+				} else {
+					console.log("Name " + playerName + " already taken");
+					$("#errMsg").html("Sorry! Name is already taken, please try again");
+				}
+			});
+			$('#waitMsg').html = "Please wait...";
+		}
+	});
+});
+
 function allowDrop(ev) {
     ev.preventDefault();
 }
--- server.js
+++ server.js
@@ -1,3 +1,9 @@
+var TEAM_SIZE = 3;
+var team[];
+var inProgress = false;
+var BOARD_SIZE;
+var trs[];
+
 // Check the configuration file for more details
 var config = require('./config');
 
@@ -90,7 +96,103 @@ io.on('connection', function (socket) { // Incoming connections from clients
   
   
   //add player to team which has space
-  socket.on('registerPlayer', function (data, playerNum, callback) {
-
+  socket.on('registerPlayer', function (data, callback) {
+	console.log("received join game request from client:", data);
+	console.log("Calling Create teams with values " + TEAM_SIZE + " " + NUM_TEAMS);
+	
+	var added = false;
+	var left;
+	
+	var teamSize = team.players.length;
+	
+	var i=0;
+	if (teamSize < TEAM_SIZE){
+		// check name is not already in one of the teams
+		if (isNameAvail(data))
+		{
+			socket.nickname = data;
+			added = true;
+			var player = {
+				name : data,
+				master : false,		
+				id : socket.id
+			};
+			if(teams[i].players.length === 0) {
+				console.log("Adding spymaster: " + data + " " + socket.id);
+				player.master = true;
+			}
+			team.players.push(player);
+			sendTeams();
+			callback (true);
+			teamSize++;
+			left = (TEAM_SIZE*NUM_TEAMS) - teamSize;
+			io.sockets.emit('teamSize', {teamSize : teamSize, left : left} );
+			
+			//If team size is reached the max
+			if (teamSize == TEAM_SIZE) {
+				
+				// If no game in progress then create the board
+				if(!inProgress) {
+					createBoard();
+				}
+				
+				//Show countdown timer then start game
+				var secs;
+				var duration = readyDuration;
+				readyTimer = setInterval(function () {
+					secs = parseInt(duration % 60, 10);
+					io.sockets.emit('getReady', secs);
+					duration--;
+					
+					if(duration < 0) {
+						clearInterval(readyTimer);
+						duration = readyDuration;
+						console.log("Calling startGame()");
+						startGame();
+					}
+					
+				}, 1000);
+			}
+		} else {
+			callback(false);
+		}
+	} else {
+		//game is full
+		console.log("Game Full");
+		socket.emit('gameFull', teams);
+	}
   });
 });
+
+function isNameAvail(data) {
+	console.log("Checking if name " + data + " is available");
+	var isAvail = true;
+	if(team.players[data] != -1) {
+		console.log("Player " + data + " already exists");
+		isAvail = false;
+	}
+	return isAvail;
+}
+
+// Function to create a new board
+function createBoard(){	
+
+	//get seed and set the seed for randomizer
+	//var gameHash = Math.floor((Math.random() * maxHash) + 1);
+	
+	//Math.seedrandom(gameHash);
+
+	//reset state to pristine state
+	//sessionData = data.slice(0);
+	
+	//Fill wordsSelected array & create 5 table rows with 5 element 
+	var output = "<table><tbody>";
+	if (!trs[i%5]){
+		trs[i%5] = "";
+	}
+	for(var i = 0; i < BOARD_SIZE; i++){
+		trs[i%5] += "<td><div class=\"imgDiv\" ondrop=\"drop(event)\" ondragover=\"allowDrop(event)\"></div></td>";
+	}
+	output += trs;
+	output +=  "</tbody></table>";
+}
