5ce960bb8792dc9f5e06c8d148a38d93e6f377d6
Anup Parekh <anup.parekh@aexp.com>
2016-06-23 21:05:23 +0100

    comment

--- client.js
+++ client.js
@@ -37,3 +37,26 @@ function drop(ev) {
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
 }
+
+// Connect event for web sockets
+function connect() {
+  
+	var connString = config.protocol + config.domain + ':' + config.clientport;
+	console.log("Websocket connection string:", connString, config.wsclientopts);
+	socket = io.connect(connString, config.wsclientopts);
+
+	// Handle error event
+	socket.on('error', function (err) {  
+	console.log("Websocket 'error' event:", err);
+	});
+
+	// Handle connection event
+	socket.on('connect', function () { 
+	console.log("Websocket 'connected' event with params:", socket);
+	});
+
+	// Handle disconnect event
+	socket.on('disconnect', function () {
+		console.log("Websocket client 'disconnect' event");
+	});
+}
