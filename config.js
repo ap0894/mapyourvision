7ebb37b4ae92b3db5981bfa44f7e63e9d59cadb1
Anup Parekh <Anup@Anups-MacBook-Pro.local>
2018-01-14 11:02:19 +0000

    first commit

--- config.js
+++ config.js
@@ -1,18 +1,20 @@
 var Config = {
   // Contents of this file will be send to the client
   //"domain":     process.env.OPENSHIFT_APP_DNS || '127.0.0.1',
-  //"domain":		'brandmaster.herokuapp.com',
+  "domain":             'mapyourvision.herokuapp.com',
 
- // "serverip":   process.env.OPENSHIFT_NODEJS_IP || process.env.OPENSHIFT_INTERNAL_IP || 'localhost',
-  //"serverport": process.env.OPENSHIFT_NODEJS_PORT ||  process.env.OPENSHIFT_INTERNAL_PORT || process.env.PORT || '3000',
-  
+  "serverip":   process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+  "serverport": process.env.PORT || 5000,
+  //"serverip":   process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
+  //"serverport": process.env.OPENSHIFT_NODEJS_PORT || '8080',
+
   //"clientport": (process.env.OPENSHIFT_NODEJS_PORT) ? '8443':'8080',
-  //"clientport": '8443',
-  //"protocol":   'wss://',
+  "clientport": '8443',
+  "protocol":   'wss://',
 
-  "heartbeattmo": 1000, // milliseconds 
-  
-  "wsclientopts": { reconnection: true, 
+  "heartbeattmo": 1000, // milliseconds
+
+  "wsclientopts": { reconnection: true,
                     reconnectionDelay: 2000,
                     reconnectionAttempts: 100,
                     secure: true
