bd53c02393c6f5c686be8af866b2c6c731327171
Anup Parekh <Anup@Anups-MacBook-Pro.local>
2018-01-14 10:44:54 +0000

    first commit

--- config.js
+++ config.js
@@ -1,11 +1,13 @@
 var Config = {
   // Contents of this file will be send to the client
   //"domain":     process.env.OPENSHIFT_APP_DNS || '127.0.0.1',
+  //"domain":		'brandmaster.herokuapp.com',
 
-  //"serverip":   process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
-  //"serverport": process.env.OPENSHIFT_NODEJS_PORT || '8080',
+ // "serverip":   process.env.OPENSHIFT_NODEJS_IP || process.env.OPENSHIFT_INTERNAL_IP || 'localhost',
+  //"serverport": process.env.OPENSHIFT_NODEJS_PORT ||  process.env.OPENSHIFT_INTERNAL_PORT || process.env.PORT || '3000',
   
   //"clientport": (process.env.OPENSHIFT_NODEJS_PORT) ? '8443':'8080',
+  //"clientport": '8443',
   //"protocol":   'wss://',
 
   "heartbeattmo": 1000, // milliseconds 
@@ -13,7 +15,7 @@ var Config = {
   "wsclientopts": { reconnection: true, 
                     reconnectionDelay: 2000,
                     reconnectionAttempts: 100,
-                    secure: false
+                    secure: true
                   }
 };
 
